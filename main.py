21443d5f9abe927552d58a100af13d1ed3f959a2
Gevorg Chobanyan <gavjan@github.com>
2021-05-05 16:56:43 +0200

    error notification

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from __future__ import print_function
 import discord
+import sys
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 
 def get_env(name):
@@ -20,15 +26,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith("./hello"):
-        await message.channel.send("Hello!")
-    if message.content.startswith("./restart_tiko"):
-        exit(0)
-    if message.content.startswith("./test_err"):
-        tmp = 0/0
+    try:
+        if message.author == client.user:
+            return
+        if message.content.startswith("./hello"):
+            await message.channel.send("Hello!")
+        if message.content.startswith("./restart_tiko"):
+            exit(0)
+        if message.content.startswith("./test_err"):
+            tmp = 0 / 0
+
+    except Exception as e:
+        eprint(e)
         exit(1)
+
 
 
 client.run(BOT_TOKEN)
