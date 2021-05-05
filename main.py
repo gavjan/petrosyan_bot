80a4d766a18c58845e7e67c7c54745ff16995e9d
gavjan <gavjan@github.com>
2021-05-05 16:14:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+
+
+def get_env(name):
+    f = open(name, "r")
+    val = f.read()
+    f.close()
+    return val
+
+
+BOT_TOKEN = get_env("bot_token")
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f"logged in as {client}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("./hello"):
+        await message.channel.send("Hello!")
+    if message.content.startswith("./restart_tiko"):
+        exit(0)
+    if message.content.startswith("./test_err"):
+        exit(0/0)
+
+
+client.run(BOT_TOKEN)
---
+++ run.sh
@@ -0,0 +1,21 @@
+send_mail() {
+  sed -i "s/\"/'/g" .err_file
+  curl "https://api.postmarkapp.com/email" \
+  -X POST \
+  -H "Accept: application/json" \
+  -H "Content-Type: application/json" \
+  -H "X-Postmark-Server-Token: $(cat .env/mail_token)" \
+  -d "{
+  \"From\": \"$(cat .env/mail)\",
+  \"To\": \"$(cat .env/mail)\",
+  \"Subject\": \"[Petrosyan] Bot Error\",
+  \"TextBody\": \"$(cat .err_file)\",
+  \"MessageStream\": \"outbound\"
+  }"
+}
+
+
+while true; do
+  python3 main.py 2>.err_file || send_mail "$(cat .err_file)"
+  git pull
+done
