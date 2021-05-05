0e9ac51d71f2732668c503802a6e42efa629a02f
gavjan <gavjan@github.com>
2021-05-05 19:14:23 +0200

    add

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("always fair !‎‎‎‎/help_tiko"))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("always fair ! /help_tiko"))
     print(f"logged in as {client}")
 
 
