38fbc01cf9e5a66c0e17e71df3172c5429a7722d
gavjan <gavjan@github.com>
2021-05-05 17:44:15 +0200

    almost ready

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from __future__ import print_function
 import discord
 import sys
+from random import randint
 
 ADMIN_ID = 213341816324489217
+KEYWORDS = {"pipi", "pampers", "tigran", "petrosian", "petrosyan"}
+DONT_COMMENT_KEYWORD = "!nopipi"
+
+PASTA = """Are you kidding ??? What the **** are you talking about man ? You are a biggest looser i ever seen in my life ! You was doing PIPI in your pampers when i was beating players much more stronger then you! You are not proffesional, because proffesionals knew how to lose and congratulate opponents, you are like a girl crying after i beat you! Be brave, be honest to yourself and stop this trush talkings!!! Everybody know that i am very good blitz player, i can win anyone in the world in single game! And "w"esley "s"o is nobody for me, just a player who are crying every single time when loosing, ( remember what you say about Firouzja ) !!! Stop playing with my name, i deserve to have a good name during whole my chess carrier, I am Officially inviting you to OTB blitz match with the Prize fund! Both of us will invest 5000$ and winner takes it all!
+I suggest all other people who's intrested in this situation, just take a look at my results in 2016 and 2017 Blitz World championships, and that should be enough... No need to listen for every crying babe, Tigran Petrosyan is always play Fair ! And if someone will continue Officially talk about me like that, we will meet in Court! God bless with true! True will never die ! Liers will kicked off...\n\n"""
+
+SHORTENED_PHRASES = [
+    "Are you kidding ??? What the **** are you talking about man ?",
+    "You was doing PIPI in your pampers when i was beating players much more stronger then you!",
+    "Be brave, be honest to yourself and stop this trush talkings!!!",
+    "Everybody know that i am very good blitz player, i can win anyone in the world in single game!",
+    '"w"esley "s"o is nobody for me',
+    "Tigran Petrosyan is always play Fair !",
+    "God bless with true! True will never die ! Liers will kicked off...",
+]
 
 
 def eprint(*args, **kwargs):
@@ -31,13 +47,19 @@ async def on_message(message):
     try:
         if message.author == client.user:
             return
-        if message.content.startswith("./hello"):
-            await message.channel.send(message.author.id == ADMIN_ID)
-        if message.content.startswith("./restart_tiko"):
-            if message.author.id == ADMIN_ID:
-                exit(0)
-        if message.content.startswith("./test_err"):
+        if message.content.startswith("./restart_tiko") and message.author.id == ADMIN_ID:
+            exit(0)
+        if message.content.startswith("./test_err") and message.author.id == ADMIN_ID:
             tmp = 0 / 0
+
+        if "!petrosyan" in message.content:
+            await message.channel.send(SHORTENED_PHRASES[randint(0, len(SHORTENED_PHRASES))])
+
+        content = message.content.lower()
+
+        for keyword in KEYWORDS:
+            if keyword in content:
+                await message.channel.send(PASTA)
 
     except Exception as e:
         eprint(e)
